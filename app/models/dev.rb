88eb4a6ccaab6fd9ffbd289ba512a6fb2fd079b5
Joshua Campos <jdeveloper1203@gmail.com>
2022-10-20 01:57:42 -0400

    finished

--- app/models/dev.rb
+++ app/models/dev.rb
@@ -1,3 +1,17 @@
 class Dev < ActiveRecord::Base
+    has_many :freebies
+    has_many :companies, through: :freebies
+
+    def received_one?(item_name)
+        if self.freebies.where("item_name == ?" , item_name).empty?
+            false
+        else
+            true
+        end
+    end
+
+    def give_away(dev, freebie)
+        freebie.update(dev_id: dev.id) unless freebie.dev_id != self.id
+    end
 
 end
